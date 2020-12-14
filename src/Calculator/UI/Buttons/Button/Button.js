18bc9f19801f812a09dd7cb2720a42d511609159
Igor <igor06brw@gmail.com>
2020-12-14 23:52:25 +0100

    init calculator

---
+++ src/Calculator/UI/Buttons/Button/Button.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const button = (props) => { return <div data-target="props.type" className={props.operator}>{props.sign}</div> }
+
+export default button;
---
+++ src/Calculator/UI/Buttons/Buttons.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Button from './Button/Button'
+
+const Buttons = (props) => {
+    return (
+        <div>
+          <Button></Button>  
+        </div>
+    );
+}
+
+export default Buttons;
