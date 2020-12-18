a2324ef00d611876131b64b370c9175c1b04916c
Igor <igor06brw@gmail.com>
2020-12-18 12:35:45 +0100

    function with condition with only one decimal in number

--- src/Calculator/Calculator.js
+++ src/Calculator/Calculator.js
@@ -5,11 +5,44 @@ import './Calculator.css'
 
 class Calculator extends Component {
     state = {
-
+        currentValue: '',
+        nextValue: '',
+        isDecimal: false
     }
 
+    componentDidUpdate() {
+        console.log(this.state);
+    }
+
+
+
+    
+
+    assignDigit(e) {
+        let tempString = this.state.currentValue;
+        tempString += e;
+        this.setState({currentValue: tempString});
+    } 
+    assignValue(number) {
+        let currentValue = this.state.currentValue
+        if (number === '.') { 
+            this.assignDecimal(number)
+        } else {
+            this.assignDigit(number)
+        }
+
+    }
+    assignDecimal(decimal) {
+        if(!this.state.isDecimal) {
+            this.setState({isDecimal: true});
+            this.assignDigit(decimal)
+        } else {
+            return null;
+        }
+    }
     onNumberHandler = (e) => {
-        console.log(e)
+        this.assignValue(e)
+        console.log(this.state);
     }
     render() {
         return (
